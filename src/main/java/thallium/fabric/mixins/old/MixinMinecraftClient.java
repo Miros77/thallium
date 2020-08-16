6812bb8ae81e4540d2d678c9e1ed385e436e8214
IsaiahPatton <isaiahpatton1@gmail.com>
2020-08-16 15:26:54 -0400

    This was disabled so lets remove it

--- src/main/java/thallium/fabric/mixins/old/MixinMinecraftClient.java
+++ src/main/java/thallium/fabric/mixins/old/MixinMinecraftClient.java
@@ -6,8 +6,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import thallium.fabric.ThalliumMod;
-import thallium.fabric.gui.ThalliumOptions;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.CloudRenderMode;
 import net.minecraft.client.options.GameOptions;
@@ -23,11 +21,6 @@ public class MixinMinecraftClient {
     @Shadow public String fpsDebugString;
     @Shadow public long nextDebugInfoUpdateTime;
 
-    private long last;
-
-    private int a = 0;
-    private int b = 0;
-
     private long lastDebugUpdateTime;
 
     @Inject(at = @At("HEAD"), method = "run")
@@ -35,25 +28,8 @@ public class MixinMinecraftClient {
         debugUpdate(); // Redirect debug info onto separate thread
     }
 
-    @Inject(at = @At("TAIL"), method = "render")
-    public void renderTail(boolean tick, CallbackInfo ci) {
-        if (!ThalliumOptions.renderSkip) {
-            a++;
-            return;
-        }
-
-        long took = System.currentTimeMillis();
-
-        // Thallium_Mod - This is pretty much disabled now
-        if (took - last > 60) {
-            ThalliumMod.doUpdate = true;
-            last = took;
-        }
-        if ((ThalliumMod.doUpdate = (a/2)-12 < b)) a++; else b++;
-    }
-
     public void debugUpdate() {
-        // Thallium_Mod - Update the FPS debug string not during render
+        // Thallium_Mod - Update the FPS debug string not during the main render
 
         new Thread(() -> {
             while (true) {
@@ -61,10 +37,8 @@ public class MixinMinecraftClient {
                 if (currentTime - lastDebugUpdateTime >= 1000) {
                     lastDebugUpdateTime = currentTime;
                     currentFps = this.fpsCounter;
-                    this.fpsDebugString = String.format("%d fps T: %s%s%s%s B: %d" + (this.options.debugProfilerEnabled ? " D:" + a + " A:" + b : ""), currentFps, (double)this.options.maxFps == Option.FRAMERATE_LIMIT.getMax() ? "inf" : Integer.valueOf(this.options.maxFps), this.options.enableVsync ? " vsync" : "", this.options.graphicsMode.toString(), this.options.cloudRenderMode == CloudRenderMode.OFF ? "" : (this.options.cloudRenderMode == CloudRenderMode.FAST ? " fast-clouds" : " fancy-clouds"), this.options.biomeBlendRadius);
+                    this.fpsDebugString = String.format("%d fps T: %s%s%s%s B: %d", currentFps, (double)this.options.maxFps == Option.FRAMERATE_LIMIT.getMax() ? "inf" : Integer.valueOf(this.options.maxFps), this.options.enableVsync ? " vsync" : "", this.options.graphicsMode.toString(), this.options.cloudRenderMode == CloudRenderMode.OFF ? "" : (this.options.cloudRenderMode == CloudRenderMode.FAST ? " fast-clouds" : " fancy-clouds"), this.options.biomeBlendRadius);
                     this.fpsCounter = 0;
-                    this.a = 0;
-                    this.b = 0;
                 }
                 try {
                     Thread.sleep(1000);
